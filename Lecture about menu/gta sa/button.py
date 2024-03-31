0b8e075dfdc337213bd467caac82dc5639db4fce
nurmatto <156771225+nurmatto@users.noreply.github.com>
2024-04-01 02:58:25 +0500

    Add files via upload

---
+++ Lecture about menu/gta sa/button.py
@@ -0,0 +1,30 @@
+import pygame
+pygame.init()
+
+# Загрузка музыкального файла
+pygame.mixer.music.load('mus.mp3')
+
+class Knopka():
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.prev_hover = False  # Флаг для отслеживания предыдущего состояния наведения
+
+    def draw(self, surface):
+        # Получаем текущее положение мыши
+        pos = pygame.mouse.get_pos()
+
+        # Проверяем, находится ли курсор мыши над кнопкой
+        over_or_not = self.rect.collidepoint(pos)
+
+        # Проверяем условия наведения мыши и нажатия кнопки
+        action = False
+        if over_or_not and pygame.mouse.get_pressed()[0]:
+            action = True
+            pygame.mixer.music.play()  # Проигрываем музыку
+
+        # Отображаем кнопку на экране
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+        
+        return action
---
+++ Lecture about menu/gta sa/gta sa.py
@@ -0,0 +1,167 @@
+import pygame 
+import random
+import button
+pygame.init()
+
+W, H = 1200, 800
+FPS = 60
+
+screen = pygame.display.set_mode((W, H), pygame.RESIZABLE)
+clock = pygame.time.Clock()
+nurma = True
+bg = (0, 0, 0)
+
+#paddle
+paddleW = 150
+paddleH = 25
+paddleSpeed = 20
+paddle = pygame.Rect(W // 2 - paddleW // 2, H - paddleH - 30, paddleW, paddleH)
+
+#Ball
+ballRadius = 20
+ballSpeed = 6
+ball_rect = int(ballRadius * 2 ** 0.5)
+ball = pygame.Rect(random.randrange(ball_rect, W - ball_rect), H // 2, ball_rect, ball_rect)
+dx, dy = 1, -1
+
+#Game score
+game_score = 0
+game_score_fonts = pygame.font.SysFont('comicsansms', 40)
+game_score_text = game_score_fonts.render(f'Your game score is: {game_score}', True, (0, 0, 0))
+game_score_rect = game_score_text.get_rect()
+game_score_rect.center = (210, 20)
+
+#Catching sound
+collision_sound = pygame.mixer.Sound('/Users/nurmatto/Downloads/catch.mp3')
+
+def detect_collision(dx, dy, ball, rect):
+    if dx > 0:
+        delta_x = ball.right - rect.left
+    else:
+        delta_x = rect.right - ball.left
+    if dy > 0:
+        delta_y = ball.bottom - rect.top
+    else:
+        delta_y = rect.bottom - ball.top
+
+    if abs(delta_x - delta_y) < 10:
+        dx, dy = -dx, -dy
+    if delta_x > delta_y:
+        dy = -dy
+    elif delta_y > delta_x:
+        dx = -dx
+    return dx, dy
+
+#block settings
+block_list = [pygame.Rect(10 + 120 * i, 50 + 70 * j, 100, 50) for i in range(10) for j in range (4)]
+color_list = [(random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255)) for i in range(10) for j in range(4)] 
+
+#Game over Screen
+losefont = pygame.font.SysFont('comicsansms', 40)
+losetext = losefont.render('Game Over', True, (255, 255, 255))
+losetextRect = losetext.get_rect()
+losetextRect.center = (W // 2, H // 2)
+
+#Win Screen
+winfont = pygame.font.SysFont('comicsansms', 40)
+wintext = losefont.render('You win yay', True, (0, 0, 0))
+wintextRect = wintext.get_rect()
+wintextRect.center = (W // 2, H // 2)
+
+
+# 1 
+paused = False
+
+# Background surface
+bg_surface = pygame.Surface((W, H))
+bg_surface.fill(bg)
+
+# 2 buttons
+gta_pause = pygame.image.load('main menu.png')
+gta_button1 = pygame.image.load('start.png')
+gta_button2 = pygame.image.load('quit.png')
+gta_button3 = pygame.image.load('option.png')
+
+# 3
+start_image = pygame.transform.scale(gta_button1, (500,40))
+quit_image = pygame.transform.scale(gta_button2, (475, 21))
+option_image = pygame.transform.scale(gta_button3, (475, 21))
+
+# 4 
+start_button = button.Knopka(280, 350, start_image)
+quit_button = button.Knopka(290, 460, quit_image)
+option_button = button.Knopka(288, 410, option_image)
+        
+
+while nurma:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            nurma = False
+        # 5
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                paused = not paused
+
+    screen.fill(bg)
+    # 6
+    if not paused:
+        # Your game code here
+        [pygame.draw.rect(screen, color_list[color], block) for color, block in enumerate(block_list)]  # drawing blocks
+        pygame.draw.rect(screen, pygame.Color(255, 255, 255), paddle)
+        pygame.draw.circle(screen, pygame.Color(255, 0, 0), ball.center, ballRadius)
+
+        ball.x += ballSpeed * dx
+        ball.y += ballSpeed * dy
+
+        # Collision left 
+        if ball.centerx < ballRadius or ball.centerx > W - ballRadius:
+            dx = -dx
+        # Collision top
+        if ball.centery < ballRadius + 50:
+            dy = -dy
+        # Collision with paddle
+        if ball.colliderect(paddle) and dy > 0:
+            dx, dy = detect_collision(dx, dy, ball, paddle)
+
+        # Collision blocks
+        hitIndex = ball.collidelist(block_list)
+
+        if hitIndex != -1:
+            hitRect = block_list.pop(hitIndex)
+            hitColor = color_list.pop(hitIndex)
+            dx, dy = detect_collision(dx, dy, ball, hitRect)
+            game_score += 1
+            collision_sound.play()
+
+        # Game score
+        game_score_text = game_score_fonts.render(f'Your game score is: {game_score}', True, (255, 255, 255))
+        screen.blit(game_score_text, game_score_rect)
+
+        # Win/lose screens
+        if ball.bottom > H:
+            screen.fill((0, 0, 0))
+            screen.blit(losetext, losetextRect)
+        elif not len(block_list):
+            screen.fill((255, 255, 255))
+            screen.blit(wintext, wintextRect)
+
+        # Paddle Control
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT] and paddle.left > 0:
+            paddle.left -= paddleSpeed
+        if key[pygame.K_RIGHT] and paddle.right < W:
+            paddle.right += paddleSpeed
+    # 7
+    else:
+        screen.blit(gta_pause, (0, 0))
+        if start_button.draw(screen) == True:
+            paused = False
+        if quit_button.draw(screen) == True:
+            nurma = False
+        if option_button.draw(screen) == True:
+            pass
+        
+    pygame.display.flip()
+    clock.tick(FPS)
+
+pygame.quit()
